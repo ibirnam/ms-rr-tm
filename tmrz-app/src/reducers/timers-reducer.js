c686297d3705b32c3fff0e186768b9d4326f485c
Ian Birnam <ianbirnam@Ians-MacBook-Pro.local>
2019-02-25 11:15:58 -0800

    formatting

--- tmrz-app/src/reducers/timers-reducer.js
+++ tmrz-app/src/reducers/timers-reducer.js
@@ -1,56 +1,58 @@
-import { NEW_TIMER, RESET_TIMER, DELETE_TIMER,
-    START_TIMER, STOP_TIMER, TOGGLE_TIMER, UPDATE } from '../actions';
+import {
+  NEW_TIMER, RESET_TIMER, DELETE_TIMER,
+  START_TIMER, STOP_TIMER, TOGGLE_TIMER, UPDATE
+} from '../actions';
 
 import Timer from '../Timer'
 //                                action = {type:'TOGGLE_TIMER', payload: 1}
 const timerReducer = (state = [], action) => {
-switch (action.type) {
+  switch (action.type) {
 
-case NEW_TIMER:
-  const name = action.payload.name ? action.payload.name : `Timer ${state.length}`
-  return [...state, new Timer(name)] // --> [33, 0]
+    case NEW_TIMER:
+      const name = action.payload.name ? action.payload.name : `Timer ${state.length}`
+      return [...state, new Timer(name)] // --> [33, 0]
 
-case RESET_TIMER:
-  return state.map((timer, index) => {
-    if (action.payload.index === index) {
-      timer.time = 0
-    }
-    return timer
-  })
+    case RESET_TIMER:
+      return state.map((timer, index) => {
+        if (action.payload.index === index) {
+          timer.time = 0
+        }
+        return timer
+      })
 
-case DELETE_TIMER:
-  return state.filter((timer, index) => {
-    return action.payload.index !== index
-  })
+    case DELETE_TIMER:
+      return state.filter((timer, index) => {
+        return action.payload.index !== index
+      })
 
-case START_TIMER:
-  return state.map((timer, index) => {
-    if (action.payload.index === index) {
-      timer.isRunning = true
-    }
-    return timer
-  })
+    case START_TIMER:
+      return state.map((timer, index) => {
+        if (action.payload.index === index) {
+          timer.isRunning = true
+        }
+        return timer
+      })
 
-case STOP_TIMER:
-  return state.map((timer, index) => {
-    if (action.payload.index === index) {
-      timer.isRunning = false
-    }
-    return timer
-  })
+    case STOP_TIMER:
+      return state.map((timer, index) => {
+        if (action.payload.index === index) {
+          timer.isRunning = false
+        }
+        return timer
+      })
 
-case TOGGLE_TIMER:
-  const newState = state.map((timer, index) => {
-    if (action.payload.index === index) {
-      return {...timer, isRunning: !timer.isRunning}
-    }
-    return timer
-  })
-  return newState
+    case TOGGLE_TIMER:
+      const newState = state.map((timer, index) => {
+        if (action.payload.index === index) {
+          return { ...timer, isRunning: !timer.isRunning }
+        }
+        return timer
+      })
+      return newState
 
-default:
-  return state;
-}
+    default:
+      return state;
+  }
 }
 
 export default timerReducer;
